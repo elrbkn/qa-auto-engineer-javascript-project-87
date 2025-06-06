27b14c3e7270c13b4fe2cb6b857c038234d2c311
Елена Рыбкина <rybkina01@list.ru>
2025-06-06 14:31:20 +0300

    Refactor: remove duplicated buildDiff and mark hotspot safe

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,31 +1,6 @@
-import _ from 'lodash';
 import parseFile from './parsers.js';
 import format from './formatters/index.js';
-
-const buildDiff = (data1, data2) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
-
-  return keys.map((key) => {
-    if (!_.has(data1, key)) {
-      return { key, type: 'added', value: data2[key] };
-    }
-
-    if (!_.has(data2, key)) {
-      return { key, type: 'removed', value: data1[key] };
-    }
-
-    if (_.isEqual(data1[key], data2[key])) {
-      return { key, type: 'unchanged', value: data1[key] };
-    }
-
-    return {
-      key,
-      type: 'updated',
-      oldValue: data1[key],
-      newValue: data2[key],
-    };
-  });
-};
+import buildDiff from './buildDiff.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const data1 = parseFile(filepath1);
